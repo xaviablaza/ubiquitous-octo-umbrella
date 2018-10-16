5992d04f48339af63c4199f7ac99de6dc3cbb466
Xavi Ablaza <xlablaza@gmail.com>
2018-10-15 19:47:07 -0700

    Update stealth-start.sh

--- stealth-start.sh
+++ stealth-start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo nvidia-docker run --rm -d -it -u $(id -u):$(id -g) --name xmr-stak -v "$PWD":/mnt xaviablaza/xmr-stak --config config.txt --noCPU
+nvidia-docker run --rm -d -it -u $(id -u):$(id -g) --name xmr-stak -v "$PWD":/mnt xaviablaza/xmr-stak --config config.txt --noCPU
