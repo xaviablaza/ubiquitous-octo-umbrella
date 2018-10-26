b06a0ec442cf339552c5478e65ed601d373a544b
Xavi Ablaza <xlablaza@gmail.com>
2018-10-25 23:57:32 -0700

    Add cron start script

---
+++ cron-start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set +e
+docker pull xaviablaza/xmr-stak
+cd /home/xavi/ubiquitous-octo-umbrella && git reset --hard && git clean --force && git pull origin master
+docker stop xmr-stak
+nvidia-docker run --rm -u $(id -u):$(id -g) --name xmr-stak -v "$PWD":/mnt xaviablaza/xmr-stak --config config.txt
